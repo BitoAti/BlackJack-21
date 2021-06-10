001a93a87f9e3b26bfbf0c72325c07575d807231
editvgny <edit.vgny@gmail.com>
2021-06-10 09:38:49 +0200

    Formatted and removed blank lines

--- card/CardDeck.php
+++ card/CardDeck.php
@@ -1,37 +1,33 @@
 <?php
 
 
-class CardDeck
-{
+class CardDeck {
     private $deck = array();
 
-    public function addCardToDeck($card){
-        array_push($this->deck,$card);
+    public function addCardToDeck($card) {
+        array_push($this->deck, $card);
     }
 
-    public function getCards(){
-        foreach ($this->deck as $card){
+    public function getCards() {
+        foreach ($this->deck as $card) {
             $card->getCard();
         }
     }
 
-    public function dealCard($index){
+    public function dealCard($index) {
         $randomCard = $this->deck[$index];
         unset($this->deck[$index]);
         return $randomCard;
     }
 
-
     /**
      * @return array
      */
-    public function getDeck(): array
-    {
+    public function getDeck(): array {
         return $this->deck;
     }
 
-
-    public function getNumberOfCardsInDeck(){
+    public function getNumberOfCardsInDeck(): int {
         return count($this->deck);
     }
 }
