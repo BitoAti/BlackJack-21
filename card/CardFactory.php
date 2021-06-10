48a7f862b21356f7b819097137f2e037907ca157
editvgny <edit.vgny@gmail.com>
2021-06-10 09:38:57 +0200

    Formatted and removed blank lines

--- card/CardFactory.php
+++ card/CardFactory.php
@@ -1,11 +1,9 @@
 <?php
 
 
-class CardFactory
-{
+class CardFactory {
 
-    static public function createCardDeck($cardDeck)
-    {
+    static public function createCardDeck($cardDeck) {
         $symbols = array('♠', '♦', '♥', '♣');
         $values = array("2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A");
 
@@ -14,11 +12,7 @@ class CardFactory
                 for($i=0;$i <2;$i++){
                     $cardDeck->addCardToDeck(new Card($symbol,$value));
                 }
-
             }
         }
-
-
     }
-
 }
